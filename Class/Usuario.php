5f10a2da71d40505c0c73b59a54214d7982fceed
dahora328 <rodrigoliveira328@hotmail.com>
2020-01-01 10:11:54 -0300

    Classe Usuario implementado metodo login

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -32,7 +32,7 @@ class Usuario{
     
     public function LoadById($id){
         $sql = new Sql();
-        $resultado = $sql->Select("SELECT * FROM usuario u where idusu = :id", array(
+        $resultado = $sql->Select("SELECT * FROM usuario where idusu = :id", array(
             ":id"=>$id));
         if(isset($resultado[0])){
             $row = $resultado[0];
@@ -41,6 +41,35 @@ class Usuario{
             $this->setSenha($row['senha']);
             $this->setDtCadastro(new DateTime($row['dtcadastro']));
         }
+    }
+    public function Login($login, $senha){
+        $sql = new Sql();
+        $resultado = $sql->Select("SELECT * FROM usuario WHERE login = :login AND senha = :senha", array(
+            ":login"=>$login,
+            ":senha"=>$senha
+        ));
+        if(isset($resultado[0])){
+            $row = $resultado[0];
+            $this->setId(utf8_encode($row['idusu']));
+            $this->setLogin(utf8_encode($row['login']));
+            $this->setSenha($row['senha']);
+            $this->setDtCadastro(new DateTime($row['dtcadastro']));
+        }else{
+            throw new Exception("Login e/ou senha inválidos.");
+            
+        }
+    }
+
+    public static function getList(){
+        $sql = new Sql();
+        return $sql->Select("SELECT * FROM usuario ORDER BY idusu");
+    }
+
+    public static  function BusacarLogin($login){
+        $sql =  new Sql();
+        return $sql->Select("SELECT * FROM usuario WHERE login LIKE :buscar ORDER BY login", array(
+            ":buscar"=>"%".$login."%"
+        ));
     }
 
     public function __toString(){
--- Index.php
+++ Index.php
@@ -2,5 +2,9 @@
 require_once("Config.php");
 $usuario = new Usuario();
 $usuario->LoadById(1);
+$lista = Usuario::getList();
+$busca = Usuario::BusacarLogin("jo");
+$usuario->login("dahora","123456");
+//echo json_encode($busca);
 echo $usuario;
 ?>
